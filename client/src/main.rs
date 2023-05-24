1a9c13e8f6c7a94aadb17d8b4bace6b8ba3cdf99
songwei163 <songweicpp@163.com>
2023-05-24 22:23:59 +0800

    quinn

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,5 @@
 use hbb_common::{
-    anyhow::Context,
+    anyhow::{Context, anyhow},
     bytes::Bytes,
     message_proto::{
         message, EncodedVideoFrame, EncodedVideoFrames, Message as FrameMessage, VideoFrame,
@@ -10,18 +10,45 @@ use hbb_common::{
 };
 use image::io::Reader as ImageReader;
 use std::time::{self, Instant};
+use std::{
+    net::SocketAddr,
+    sync::Arc,
+};
+
+use hbb_common::{
+    // message_proto::{message, Message as FrameMessage},
+    // protobuf::Message,
+    // quic::{server, Connection, ReceiveAPI, SendAPI},
+    // ResultType, anyhow::anyhow,
+    bytes_codec::BytesCodec
+};
+// use quinn::{Endpoint, ServerConfig};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tracing::{error, info, info_span};
+use tracing_subscriber;
+use tokio_util::codec::{FramedRead, FramedWrite};
+use futures::{
+    sink::SinkExt,
+    stream::{SplitSink, SplitStream, StreamExt},
+};
+use quinn::{ClientConfig, Endpoint, VarInt};
+use std::{error::Error, net::ToSocketAddrs};
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::signal::unix::{signal, SignalKind};
+// use url::Url;
 
 const BIND_INTERFACE: &str = "0.0.0.0";
-const SERVER_IP: &str = "10.10.80.22";
-const SERVER_PORT: u32 = 51111;
+const SERVER_IP: &str = "127.0.0.1";
+const SERVER_PORT: u32 = 12345;
 const MARK_SEND_INTERVAL: u64 = 1;
 
 #[inline]
 pub fn get_time() -> i64 {
     std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .map(|d| d.as_millis())
-        .unwrap_or(0) as _
+    .duration_since(std::time::UNIX_EPOCH)
+    .map(|d| d.as_millis())
+    .unwrap_or(0) as _
 }
 
 #[inline]
@@ -50,57 +77,158 @@ fn create_msg(vp9s: Vec<EncodedVideoFrame>, last_send_marked: &mut Instant) -> F
     msg_out
 }
 
-#[tokio::main()]
-async fn main() -> ResultType<()> {
-    println!(
+/// Enables MTUD if supported by the operating system
+#[cfg(not(any(windows, os = "linux")))]
+pub fn enable_mtud_if_supported() -> quinn::TransportConfig {
+    let transport_config = quinn::TransportConfig::default();
+    transport_config
+}
+
+/// Enables MTUD if supported by the operating system
+#[cfg(any(windows, os = "linux"))]
+pub fn enable_mtud_if_supported() -> quinn::TransportConfig {
+    let mut transport_config = quinn::TransportConfig::default();
+    transport_config.mtu_discovery_config(Some(quinn::MtuDiscoveryConfig::default()));
+    transport_config
+}
+
+struct SkipServerVerification;
+
+impl SkipServerVerification {
+    fn new() -> Arc<Self> {
+        Arc::new(Self)
+    }
+}
+
+impl rustls::client::ServerCertVerifier for SkipServerVerification {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &rustls::Certificate,
+        _intermediates: &[rustls::Certificate],
+        _server_name: &rustls::ServerName,
+        _scts: &mut dyn Iterator<Item = &[u8]>,
+        _ocsp_response: &[u8],
+        _now: std::time::SystemTime,
+        ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
+        Ok(rustls::client::ServerCertVerified::assertion())
+        }
+        }
+
+        fn configure_client() -> ResultType<ClientConfig> {
+        let crypto = rustls::ClientConfig::builder()
+        .with_safe_defaults()
+        .with_custom_certificate_verifier(SkipServerVerification::new())
+        .with_no_client_auth();
+
+        let mut client_config = ClientConfig::new(Arc::new(crypto));
+        let mut transport_config = enable_mtud_if_supported();
+        transport_config.max_idle_timeout(Some(VarInt::from_u32(60_000).into()));
+        transport_config.keep_alive_interval(Some(std::time::Duration::from_secs(1)));
+        client_config.transport_config(Arc::new(transport_config));
+
+        Ok(client_config)
+        }
+
+        /// Constructs a QUIC endpoint configured for use a client only.
+        ///
+        /// ## Args
+        ///
+        /// - server_certs: list of trusted certificates.
+        #[allow(unused)]
+        pub fn make_client_endpoint(bind_addr: SocketAddr) -> ResultType<Endpoint> {
+        let client_cfg = configure_client()?;
+        let mut endpoint = Endpoint::client(bind_addr)?;
+        endpoint.set_default_client_config(client_cfg);
+        Ok(endpoint)
+        }
+
+        #[tokio::main()]
+        async fn main() -> ResultType<()> {
+        println!(
         "Start client demo, connecting the server {}:{}",
         SERVER_IP, SERVER_PORT
-    );
+        );
 
-    let local_addr = format!("{}:0", BIND_INTERFACE).parse().unwrap();
-    let server_addr = format!("{}:{}", SERVER_IP, SERVER_PORT).parse().unwrap();
+        let local_addr: SocketAddr = format!("{}:0", BIND_INTERFACE).parse().unwrap();
+        let server_addr: SocketAddr = format!("{}:{}", SERVER_IP, SERVER_PORT).parse().unwrap();
 
-    let mut client_conn = Connection::new_for_client_conn(server_addr, local_addr)
+        // let mut client_conn = Connection::new_for_client_conn(server_addr, local_addr)
+        //     .await
+        //     .with_context(|| "Failed to neccect the server")?;
+
+        let endpoint = make_client_endpoint(local_addr)?;
+        let connection = endpoint
+        .connect(server_addr, "localhost")?
+        .await?;
+
+        info!("11111");
+
+        let (mut send, mut recv) = connection
+        .open_bi()
         .await
-        .with_context(|| "Failed to neccect the server")?;
+        .map_err(|e| anyhow!("failed to open stream: {}", e))?;
+        info!("2222");
+        let mut framed_recv = FramedRead::new(recv, BytesCodec::new());
+        let mut framed_send = FramedWrite::new(send, BytesCodec::new());
 
-    let img = ImageReader::open("image.png")?.decode()?;
 
-    let fps = 25;
-    let spf = time::Duration::from_secs_f32(1. / (fps as f32));
-    let mut last_send_marked = time::Instant::now();
+        // loop {
+        //     tokio::select! {
+        //         bytes = framed_recv.next() => {
+        //             if let Some(Ok(bytes)) = bytes {
+        //                 if let Ok(msg_in) = FrameMessage::parse_from_bytes(&bytes) {
+        //                     match msg_in.union {
+        //                         Some(message::Union::VideoFrame(vf)) => {
+        //                             println!("E2E latency: {}", get_time() - vf.timestamp);
+        //                         }
+        //                         _ => {}
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
-    let mut conn_sender = client_conn.get_conn_sender().await?;
 
-    tokio::spawn(async move {
+        let img = ImageReader::open("image.png")?.decode()?;
+
+        let fps = 25;
+        let spf = time::Duration::from_secs_f32(1. / (fps as f32));
+        let mut last_send_marked = time::Instant::now();
+
+        // let mut conn_sender = client_conn.get_conn_sender().await?;
+
+        tokio::spawn(async move {
         loop {
-            if let Some(Ok(bytes)) = client_conn.next().await {
-                if let Ok(msg_in) = FrameMessage::parse_from_bytes(&bytes) {
-                    match msg_in.union {
-                        Some(message::Union::VideoFrame(vf)) => {
-                            println!("E2E latency: {}", get_time() - vf.timestamp);
-                        }
-                        _ => {}
-                    }
-                }
-            }
+        if let Some(Ok(bytes)) = framed_recv.next().await {
+        if let Ok(msg_in) = FrameMessage::parse_from_bytes(&bytes) {
+        match msg_in.union {
+        Some(message::Union::VideoFrame(vf)) => {
+        println!("E2E latency: {}", get_time() - vf.timestamp);
         }
-    });
+        _ => {}
+        }
+        }
+        }
+        }
+        });
 
-    loop {
+        loop {
         let now = time::Instant::now();
         let frame_data = create_frame(img.clone().into_bytes());
         let mut frames = Vec::new();
         frames.push(frame_data);
         let frames_msg = create_msg(frames, &mut last_send_marked);
-        conn_sender.send(&frames_msg).await.ok();
+        let data = Bytes::from(frames_msg.write_to_bytes()?);
+        framed_send.send(data).await.ok();
+        info!("3333");
         let elapsed = now.elapsed();
         if elapsed < spf {
-            tokio::time::sleep(spf - elapsed).await;
-            // println!("Timestamp {:?}", time::Instant::now());
+        tokio::time::sleep(spf - elapsed).await;
+        // println!("Timestamp {:?}", time::Instant::now());
         } else {
-            println!("Send slowly!!!");
+        println!("Send slowly!!!");
         }
-    }
-    return Ok(());
-}
+        }
+        return Ok(());
+        }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,57 +1,192 @@
 use hbb_common::{
     message_proto::{message, Message as FrameMessage},
     protobuf::Message,
-    quic::{server, Connection, ReceiveAPI, SendAPI},
-    tokio, ResultType,
+    // quic::{server, Connection, ReceiveAPI, SendAPI},
+    ResultType, anyhow::anyhow,
+    bytes_codec::BytesCodec
 };
 use std::time;
+use quinn::{Endpoint, ServerConfig, VarInt};
+use std::{
+    net::SocketAddr,
+    sync::Arc,
+};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tracing::{error, info, info_span};
+use tracing_subscriber;
+use tokio_util::codec::{FramedRead, FramedWrite};
+use futures::{
+    sink::SinkExt,
+    stream::{SplitSink, SplitStream, StreamExt},
+};
 
-#[tokio::main()]
-async fn main() -> ResultType<()> {
-    let port = 51111;
-    let bind_addr = format!("{}:{}", "0.0.0.0", port)
-        .parse()
-        .expect("parse signaling server address error");
-    let mut server_endpoint = server::new_server(bind_addr).expect("create server error");
-    println!("Start server demo, listening the port: {}", port);
+/// Returns default server configuration along with its certificate.
+fn configure_server() -> ResultType<(ServerConfig, Vec<u8>)> {
+    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+    let cert_der = cert.serialize_der().unwrap();
+    let priv_key = cert.serialize_private_key_der();
+    let priv_key = rustls::PrivateKey(priv_key);
+    let cert_chain = vec![rustls::Certificate(cert_der.clone())];
+
+    let mut server_config = ServerConfig::with_single_cert(cert_chain, priv_key)?;
+    let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
+    transport_config.max_concurrent_uni_streams(0_u8.into());
+    transport_config.max_idle_timeout(Some(VarInt::from_u32(60_000).into()));
+    transport_config.keep_alive_interval(Some(std::time::Duration::from_secs(1)));
+    #[cfg(any(windows, os = "linux"))]
+    transport_config.mtu_discovery_config(Some(quinn::MtuDiscoveryConfig::default()));
+
+    Ok((server_config, cert_der))
+}
+
+#[allow(unused)]
+pub fn make_server_endpoint(bind_addr: SocketAddr) -> ResultType<(Endpoint, Vec<u8>)> {
+    let (server_config, server_cert) = configure_server()?;
+    let endpoint = Endpoint::server(server_config, bind_addr)?;
+    Ok((endpoint, server_cert))
+}
+
+async fn handle_connection(conn: quinn::Connecting) -> ResultType<()> {
+    let connection = conn.await?;
+    let span = info_span!(
+        "connection",
+        remote = %connection.remote_address(),
+        protocol = %connection
+            .handshake_data()
+            .unwrap()
+            .downcast::<quinn::crypto::rustls::HandshakeData>().unwrap()
+            .protocol
+            .map_or_else(|| "<none>".into(), |x| String::from_utf8_lossy(&x).into_owned())
+    );
+
+    // let incoming_streams = connection.incoming_bi_streams
+
+    loop {
+        match connection.accept_bi().await {
+            Ok(stream) => {
+                tokio::spawn(async move {
+                    if let Err(e) = handle_request(stream).await {
+                        error!("handle request failed: {:?}", e);
+                    }
+                });
+            }
+            Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+                info!("connection closed");
+                return Ok(());
+            }
+            Err(e) => {
+                return Err(anyhow!("{:?}", e));
+            }
+        }
+    }
+}
+
+async fn handle_request(
+    (mut send, mut recv): (quinn::SendStream, quinn::RecvStream),
+) -> ResultType<()> {
+    let mut framed_recv = FramedRead::new(recv, BytesCodec::new());
+    let mut framed_send = FramedWrite::new(send, BytesCodec::new()); 
     loop {
         tokio::select! {
-            Some(mut new_conn) = server_endpoint.accept() => {
-                let client_addr = new_conn.remote_addr().unwrap();
-                tokio::spawn(async move {
-                    println!("Found new connection, client addr: {:?}", &client_addr);
-                    while let Ok(Some(stream)) = new_conn.accept_bidirectional_stream().await {
-                        println!("Accept new bidirectional stream");
-                        let mut conn = Connection::new_conn_wrapper(stream).await.unwrap();
-                        tokio::spawn(async move {
-                            loop {
-                                if let Some(result) = conn.next().await {
-                                    match result {
-                                        Err(err) => {
-                                            println!("read msg for peer {:?} with error: {:?}", &client_addr, err);
-                                            break;
-                                        }
-                                        Ok(bytes) => {
-                                            if let Ok(msg_in) = FrameMessage::parse_from_bytes(&bytes) {
-                                                match msg_in.union {
-                                                    Some(message::Union::VideoFrame(vf)) => {
-                                                        if vf.timestamp > 0 {
-                                                            let data = bytes.freeze();
-                                                            println!("forward the bytes received, timestamp: {:?}, size: {:?}", time::Instant::now(), data.len());
-                                                            conn.send_bytes(data).await.ok();
-                                                        }
-                                                    }
-                                                    _ => {}
-                                                }
+            res = framed_recv.next() => {
+                if let Some(res) = res {
+                    match res {
+                        Ok(bytes) => {
+                            match FrameMessage::parse_from_bytes(&bytes) {
+                                Ok(msg_in) => {
+                                    match msg_in.union {
+                                        Some(message::Union::VideoFrame(vf)) => {
+                                            if vf.timestamp > 0 {
+                                                let data = bytes.freeze();
+                                                info!("forward the bytes received, timestamp: {:?}, size: {:?}", time::Instant::now(), data.len());
+                                                framed_send.send(data).await.ok();
                                             }
+                                        }
+                                        _ => {
+                                            error!("msg is null");
                                         }
                                     }
                                 }
+                                Err(e) => {
+                                    error!("parse failed: {:?}", e);
+                                }
                             }
-                        });
+                        }
+                        Err(e) => {
+                            error!("recv err: {:?}", e);
+                        }
+                    }
+                } else {
+                    error!("recv null");
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main()]
+async fn main() -> ResultType<()> {
+    tracing_subscriber::fmt::init();
+    info!("->>>");
+    let addr: SocketAddr = "0.0.0.0:12345".parse().unwrap();
+    let (endpoint, _) = make_server_endpoint(addr)?;
+    loop {
+        tokio::select! {
+            Some(incoming_conn) = endpoint.accept() => {
+                tokio::spawn(async move {
+                    if let Err(e) = handle_connection(incoming_conn).await {
+                        info!("->>> {:?}", e);
                     }
                 });
             }
         }
     }
+    // Ok(())
 }
+// let port = 51111;
+//     let bind_addr = format!("{}:{}", "0.0.0.0", port)
+//         .parse()
+//         .expect("parse signaling server address error");
+//     let mut server_endpoint = server::new_server(bind_addr).expect("create server error");
+//     println!("Start server demo, listening the port: {}", port);
+//     loop {
+//         tokio::select! {
+//             Some(mut new_conn) = server_endpoint.accept() => {
+//                 let client_addr = new_conn.remote_addr().unwrap();
+//                 tokio::spawn(async move {
+//                     println!("Found new connection, client addr: {:?}", &client_addr);
+//                     while let Ok(Some(stream)) = new_conn.accept_bidirectional_stream().await {
+//                         println!("Accept new bidirectional stream");
+//                         let mut conn = Connection::new_conn_wrapper(stream).await.unwrap();
+//                         tokio::spawn(async move {
+//                             loop {
+//                                 if let Some(result) = conn.next().await {
+//                                     match result {
+//                                         Err(err) => {
+//                                             println!("read msg for peer {:?} with error: {:?}", &client_addr, err);
+//                                             break;
+//                                         }
+//                                         Ok(bytes) => {
+//                                             if let Ok(msg_in) = FrameMessage::parse_from_bytes(&bytes) {
+//                                                 match msg_in.union {
+//                                                     Some(message::Union::VideoFrame(vf)) => {
+//                                                         if vf.timestamp > 0 {
+//                                                             let data = bytes.freeze();
+//                                                             println!("forward the bytes received, timestamp: {:?}, size: {:?}", time::Instant::now(), data.len());
+//                                                             conn.send_bytes(data).await.ok();
+//                                                         }
+//                                                     }
+//                                                     _ => {}
+//                                                 }
+//                                             }
+//                                         }
+//                                     }
+//                                 }
+//                             }
+//                         });
+//                     }
+//                 });
+//             }
+//         }
+//     }
